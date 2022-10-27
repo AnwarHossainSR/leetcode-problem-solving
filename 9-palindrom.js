d6c48aed43fa1e7ea0895a158202eaad65487d7a
Anwar Hossain <mahedianwar@gmail.com>
2022-10-27 14:08:04 +0600

    palindrom problem solved

---
+++ 9-palindrom.js
@@ -0,0 +1,14 @@
+//-121 is palindrom or not?
+
+function isPalindrom(num) {
+  let temp = num;
+  let rev = 0;
+  while (temp > 0) {
+    let lastDigit = temp % 10;
+    rev = rev * 10 + lastDigit;
+    temp = Math.floor(temp / 10);
+  }
+  return rev === num;
+}
+
+isPalindrom(-121)
