a5e28fc57316814d39499c4b658793665b59d4b2
Anwar Hossain <mahedianwar@gmail.com>
2022-10-28 17:16:05 +0600

    58 - length of last word

---
+++ 58-easy-lengthOfLastWord.js
@@ -0,0 +1,7 @@
+const lengthOfLastWord = (str) => {
+  let strArr = str.trim().split(' ');
+  const lws = strArr[strArr.length - 1];
+  return lws.length;
+};
+
+console.log(lengthOfLastWord("luffy is still joyboy"));
