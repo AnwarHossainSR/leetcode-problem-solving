8a4b734386e12a400678923b67a31d9e7a4ae95c
Restyled.io <commits@restyled.io>
2022-10-30 17:33:29 +0000

    Restyled by prettier

--- 125-validPalindrom.js
+++ 125-validPalindrom.js
@@ -14,27 +14,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+var isPalindrome = function (s) {
   let str = s.toLowerCase();
-  let finalStr = '';
+  let finalStr = "";
   for (let i = 0; i < str.length; i++) {
     const res = checkAsciiValue(str[i]);
     if (res === true) {
       finalStr += str[i];
     }
   }
-  if (finalStr === finalStr.split('').reverse().join(''))
-    return true;
+  if (finalStr === finalStr.split("").reverse().join("")) return true;
   return false;
 };
 
 const checkAsciiValue = (value) => {
-  if (value.charCodeAt(0) >= 97 && value.charCodeAt(0) <= 122)
-    return true;
-  if (value.charCodeAt(0) >= 48 && value.charCodeAt(0) <= 57)
-    return true;
+  if (value.charCodeAt(0) >= 97 && value.charCodeAt(0) <= 122) return true;
+  if (value.charCodeAt(0) >= 48 && value.charCodeAt(0) <= 57) return true;
   return false;
 };
 
-console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
 // console.log(isPalindrome('0p'));
