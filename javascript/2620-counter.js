bdc2e68b2f1681e9968ed80e5b0cf10dde3eee7e
Anwar Hosain <anwar.sarker@bjitgroup.com>
2023-06-23 17:06:27 +0600

    solved counter problem

---
+++ javascript/2620-counter.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  let count = n;
+
+  function counter() {
+    const currentCount = count;
+    count += 1;
+    return currentCount;
+  }
+
+  return counter;
+};
+
+const counter = createCounter(5);
+
+console.log(counter());  // Output: 5
+console.log(counter());  // Output: 6
+console.log(counter());  // Output: 7
+console.log(counter());  // Output: 8
+console.log(counter());  // Output: 9
+console.log(counter());  // Output: 10
