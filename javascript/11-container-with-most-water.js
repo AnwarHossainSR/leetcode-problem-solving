146a558ff8397f7abfe2b7624be8970331ae6dc6
Anwar Hosain <anwar.sarker@bjitgroup.com>
2023-06-23 16:32:27 +0600

    Solved findMedianSortedArrays problem

--- javascript/11-container-with-most-water.js
+++ javascript/11-container-with-most-water.js
@@ -1,23 +1,26 @@
-function maxArea(height) {
-    let left = 0;
-    let right = height.length - 1;
-    let maxArea = 0;
-  
-    while (left < right) {
-      // Calculate the area of the current container
-      const currentArea = Math.min(height[left], height[right]) * (right - left);
-  
-      // Update the maximum area
-      maxArea = Math.max(maxArea, currentArea);
-  
-      //Move the pointer with the smaller height
-      if (height[left] < height[right]) {
-        left++;
-      } else {
-        right--;
-      }
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    // Calculate the area of the current container
+    const currentArea = Math.min(height[left], height[right]) * (right - left);
+
+    // Update the maximum area
+    maxArea = Math.max(maxArea, currentArea);
+
+    //Move the pointer with the smaller height
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
     }
-  
-    return maxArea;
   }
-  
+
+  return maxArea;
+};
