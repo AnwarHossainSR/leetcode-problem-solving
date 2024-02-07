f258a1d5c0fd9c4e033a95a157c793802b28c12f
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-02-07 11:46:16 +0600

    problem no 49

---
+++ python/49.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs):
+        if not strs:
+            return [[""]]
+        if len(strs) == 1:
+            return [strs]
+
+        groups_dict = defaultdict(list)
+
+        for value in strs:
+            sorted_value = ''.join(sorted(value))
+            groups_dict[sorted_value].append(value)
+
+        return list(groups_dict.values())
+
+
+# calling
+s = Solution()
+print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
