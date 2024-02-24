c38751de8aed4c0403de881f53d7f63bea3df1c0
Restyled.io <commits@restyled.io>
2024-02-24 17:23:12 +0000

    Restyled by pyment

--- python/26.py
+++ python/26.py
@@ -2,7 +2,13 @@ from typing import List
 
 
 class Solution:
+    """ """
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+
+        :param nums: List[int]: 
+
+        """
         i = 0
         while i < len(nums) - 1:
             if nums[i] == nums[i + 1]:
