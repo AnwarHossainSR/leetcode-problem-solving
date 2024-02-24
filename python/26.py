4c86a1f5214374561438293c9cff8dc73f279a7a
Restyled.io <commits@restyled.io>
2024-02-24 17:23:08 +0000

    Restyled by black

--- python/26.py
+++ python/26.py
@@ -4,8 +4,8 @@ from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 0
-        while i < len(nums)-1:
-            if nums[i] == nums[i+1]:
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
                 nums.pop(i)
             else:
                 i += 1
